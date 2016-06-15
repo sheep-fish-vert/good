b08198c3aa9bfd8b7abb1bd41f0f54fd6f890ffc
radiks <radion91@gmail.com>
2016-06-15 09:02:49 +0300

    wqeqe

--- src/js/develop/develop_4.js
+++ src/js/develop/develop_4.js
@@ -1,8 +1,15 @@
-
+function massonry(){
+    $('.good_grid').masonry({
+      columnWidth: 315,
+      itemSelector: '.good_grid_item',
+      gutter: 3,
+      fitWidth: true
+    });
+}
 
 
 $(document).ready(function(){
-
+    massonry();
 });
 
 $(window).load(function(){
