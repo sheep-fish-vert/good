b6e4449201856cb40553f9949eda6810677377b0
Dmitry <dmitry1939@gmail.com>
2016-06-15 10:30:41 +0300

    header checkpoint

--- src/js/develop/develop_1.js
+++ src/js/develop/develop_1.js
@@ -21,6 +21,37 @@
 
 /* /left columns second level toggle */
 
+/* form styler with img */
+
+    function formStylerWithImg(){
+
+        $('.good_select_has_img').each(function(){
+
+            var selectImg = $(this);
+
+            $(this).styler({
+                onFormStyled:function(){
+                    var parent = $(this).parents('.good_select_has_img');
+                    parent.find('li').removeAttr('style');
+                    parent.find('li').each(function(){
+                        parent.append('<img src='+$(this).attr('data-image')+' alt="" />');
+                    });
+
+                },
+                onSelectClosed:function(){
+
+                }
+            });
+
+        });
+
+
+
+
+    }
+
+/* /form styler with img */
+
 $(document).ready(function(){
 
     toggleLeftLinksSecondLevel();
@@ -28,6 +59,8 @@ $(document).ready(function(){
 });
 
 $(window).load(function(){
+
+    formStylerWithImg();
 
 });
 
