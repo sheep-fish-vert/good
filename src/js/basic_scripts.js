dab75f9344c69c4aa35c5fd6478984f2881fe906
radiks <radion91@gmail.com>
2016-06-21 10:11:39 +0300

    123213

--- src/js/basic_scripts.js
+++ src/js/basic_scripts.js
@@ -97,31 +97,6 @@ function cutText(){
 
 //bind example
 
-function bindExample(){
-
-    $(document).bind('click', bindFunc);
-
-    function bindFunc(){
-
-        $(document).unbind('click');
-
-        console.log('unbinded');
-
-        $.ajax({
-            url:'ajax.php',
-            method:'POST',
-            success:function(){
-
-                setTimeout(function(){
-                    console.log('binded');
-                    $(document).bind('click', bindFunc);
-                },4000);
-
-            }
-        });
-    };
-
-}
 
 function numOnly(){
 
